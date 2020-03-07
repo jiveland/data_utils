eec06dd2190fdcc296ec4b01a0519bfa6382c38e
jiveland <60073183+jiveland@users.noreply.github.com>
2020-03-06 19:08:23 -0800

    Update subtract_concat_loops.py
    remove additional rows

--- subtract_concat_loops.py
+++ subtract_concat_loops.py
@@ -4,9 +4,9 @@ import numpy as np
 #import dataframe
 df=pd.read_excel('Example_Data.xlsx')
 print(df.head(200))
-dev_list = df['Device'].unique()
-#list of devices
-print(dev)
+dev = df['Device'].unique()
+#list of device
+print(dev_list)
 
 dfout=pd.DataFrame()
 dfout2 = pd.DataFrame()
@@ -15,31 +15,34 @@ time = [1.5,2.5]
 # addrow= {}
 data_list =['Data', 'Data Two']
 sub_list = ['Data Sub','Data Two Sub']
+
 # interpolation 
 for d in dev:
     addrow_list = []
-
-    for data in data_list:
-        dfin = df.loc[(df['Device']==d)]
-        x=dfin['Time'].values
-        y=dfin[data].values
-        fit = np.polyfit(x,y,2)
-        for t in time:
-            yfit = np.polyval(fit, t)
-            addrow_list.append({'Time':t,'Device':d,data:yfit})
-        
-    addrow = dfin.append(addrow_list, ignore_index=True)
+    for t in time:
+        row={'Time':t,'Device':d}
+        for data in data_list:
+            dfin = df.loc[(df['Device']==d)]
+            x=dfin['Time'].values
+            y=dfin[data].values
+            z = np.polyfit(x,y,2)
+            ynew=np.polyval(z,t)
+            row[data]=ynew
+        addrow_list.append(row)
+    add = dfin.append(addrow_list, ignore_index=True)
     
-    dfout = dfout.append(addrow)
+    dfout = dfout.append(add)
 print(dfout.head(45))
 
 #subtraction of 0 values
-
+data_list = []
 for d in dev:
-    dfin = dfout.loc[(dfout['Device']==d)]
+    dfin2 = dfout.loc[(dfout['Device']==d)].copy()
+    new_list=[]
     for data,sub in zip(data_list,sub_list):
-        dfin[sub] = dfin[data]-dfin.loc[dfin['Time']==0,data].values
-
-    dfout2 = dfout2.append(dfin)
+        dfin2[sub] = dfin2[data]-dfin2.loc[dfin.Time.idxmin(),data]
+    dfout2=dfout2.append(dfin2)
     
 print(dfout2.head(20))
+
+
